af5e67abf9530c2cbe9afa00aa3027867802a637
wilfredloh <lohwilfred19@gmail.com>
2019-09-07 17:45:42 +0800

    added hashing of url so that users can share public urls of finished cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :moments
+
+  get '/m/:hash' => 'moments#x' 
+
   devise_for :users
   root 'login#index'
+  get '*path' => redirect('/')
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
